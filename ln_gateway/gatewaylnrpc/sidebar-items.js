628aba6aa24b128ae49b0a9c4921c6b1271b272d
otech47 <otech47@users.noreply.github.com>
2023-08-30 21:58:58 +0000

    deploy: 23a297cc1ea74050813efc2333c2b53c37ef13b2

--- ln_gateway/gatewaylnrpc/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-window.SIDEBAR_ITEMS = {"mod":["gateway_lightning_client","gateway_lightning_server","get_route_hints_response","intercept_htlc_response"],"struct":["EmptyRequest","EmptyResponse","GetNodeInfoResponse","GetRouteHintsResponse","InterceptHtlcRequest","InterceptHtlcResponse","PayInvoiceRequest","PayInvoiceResponse"]};
